c79828bd5156db3f6a0a40ebb92cfd63b1dd40e7
katherine-rock <katherine_rock@hotmail.com>
2021-06-14 15:35:02 +1000

    Update host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # KR added 3:34pm 14Jun2021
+  config.action_mailer.default_url_options = { host: 'https://whitsundays2021.herokuapp.com/' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
